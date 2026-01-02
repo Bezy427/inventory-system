5f84a71f7acb8424559bc0c84cc3f6121387aa47
Bezy427 <rusamubezel@gmail.com>
2026-01-02 13:17:14 +0200

    updated imports

--- src/main/java/com/bezy/inventorysystem/repositories/ProductRepository.java
+++ src/main/java/com/bezy/inventorysystem/repositories/ProductRepository.java
@@ -2,9 +2,7 @@ package com.bezy.inventorysystem.repositories;
 
 import com.bezy.inventorysystem.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.history.RevisionRepository;
 
-import java.util.Arrays;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
